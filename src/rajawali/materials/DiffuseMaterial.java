4cb1967fbc68e1218eb0019846095032a7dfbaa4
Jay Weisskopf <jay@jayschwa.net>
2012-10-01 16:42:31 -0500

    Preserve alpha channel in light and fog math

--- src/rajawali/materials/DiffuseMaterial.java
+++ src/rajawali/materials/DiffuseMaterial.java
@@ -79,7 +79,7 @@ public class DiffuseMaterial extends AAdvancedMaterial {
 		
 		M_FOG_FRAGMENT_CALC +
 		
-		"	gl_FragColor = uAmbientIntensity * uAmbientColor + intensity * gl_FragColor;" +
+		"	gl_FragColor.rgb = uAmbientIntensity.rgb * uAmbientColor.rgb + intensity * gl_FragColor.rgb;\n" +
 		M_FOG_FRAGMENT_COLOR +		
 		"}";
 	
@@ -118,7 +118,7 @@ public class DiffuseMaterial extends AAdvancedMaterial {
 				vc.append("vAttenuation").append(i).append(" = 1.0;\n");
 				sb.append("L = -normalize(uLightDirection").append(i).append(");\n");				
 			}
-			//sb.append("gl_FragColor += vec4(uLightColor").append(i).append(", 1.0);\n");
+			//sb.append("gl_FragColor.rgb += uLightColor").append(i).append(";\n");
 			sb.append("intensity += uLightPower").append(i).append(" * max(dot(N, L), 0.1) * vAttenuation").append(i).append(";\n");
 		}
 		
