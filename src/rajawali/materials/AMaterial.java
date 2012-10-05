eec800ba6865c172cf94c76e240b5ca83c7bcf95
Jay Weisskopf <jay@jayschwa.net>
2012-10-05 14:02:39 -0500

    Fix typo in matrix handle validity check

--- src/rajawali/materials/AMaterial.java
+++ src/rajawali/materials/AMaterial.java
@@ -400,7 +400,7 @@ public abstract class AMaterial {
 
 	public void setViewMatrix(float[] viewMatrix) {
 		mViewMatrix = viewMatrix;
-		if(checkValidHandle(muMMatrixHandle, null))
+		if(checkValidHandle(muVMatrixHandle, null))
 			GLES20.glUniformMatrix4fv(muVMatrixHandle, 1, false, viewMatrix, 0);
 	}
 	
