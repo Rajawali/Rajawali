b8860df14a939ae52040bb4750f5dba68ef11888
Dennis Ippel <ippeldv@gmail.com>
2012-11-25 23:50:57 +0000

    Forgot this one.

---
+++ src/rajawali/parser/AParser.java
@@ -0,0 +1,85 @@
+package rajawali.parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+
+import rajawali.renderer.RajawaliRenderer;
+import android.content.res.Resources;
+import android.os.Environment;
+
+public abstract class AParser implements IParser {
+	protected Resources mResources;
+	protected int mResourceId;
+	protected String mFileOnSDCard;
+	protected File mFile;
+
+	public AParser(RajawaliRenderer renderer, String fileOnSDCard)
+	{
+		this(renderer.getContext().getResources(), 0);
+		mFileOnSDCard = fileOnSDCard;
+	}
+	
+	public AParser(RajawaliRenderer renderer, int resourceId)
+	{
+		this(renderer.getContext().getResources(), resourceId);
+	}
+	
+	public AParser(Resources resources, int resourceId) 
+	{
+		mResources = resources;
+		mResourceId = resourceId;
+	}
+	
+	public IParser parse() {
+		if(mFileOnSDCard != null) {
+			File sdcard = Environment.getExternalStorageDirectory();
+			mFile = new File(sdcard, mFileOnSDCard);
+		}
+		return this;
+	}
+	
+	protected String readString(InputStream stream) throws IOException {
+        String result = new String();
+        byte inByte;
+        while ((inByte = (byte) stream.read()) != 0)
+                result += (char) inByte;
+        return result;
+	}
+	
+	protected int readInt(InputStream stream) throws IOException {
+	        return stream.read() | (stream.read() << 8) | (stream.read() << 16)
+	                        | (stream.read() << 24);
+	}
+	
+	protected int readShort(InputStream stream) throws IOException {
+	        return (stream.read() | (stream.read() << 8));
+	}
+	
+	protected float readFloat(InputStream stream) throws IOException {
+	        return Float.intBitsToFloat(readInt(stream));
+	}
+	
+	protected String getOnlyFileName(String fileName) {
+		String fName = new String(fileName);
+		int indexOf = fName.lastIndexOf("\\");
+		if(indexOf > -1)
+			fName = fName.substring(indexOf + 1, fName.length());
+		indexOf = fName.lastIndexOf("/");
+		if(indexOf > -1)
+			fName = fName.substring(indexOf + 1, fName.length());
+		return fName.toLowerCase(Locale.ENGLISH).replaceAll("\\s", "_");
+	}
+	
+	protected String getFileNameWithoutExtension(String fileName) {
+		String fName = fileName.substring(0, fileName.lastIndexOf("."));
+		int indexOf = fName.lastIndexOf("\\");
+		if(indexOf > -1)
+			fName = fName.substring(indexOf + 1, fName.length());
+		indexOf = fName.lastIndexOf("/");
+		if(indexOf > -1)
+			fName = fName.substring(indexOf + 1, fName.length());
+		return fName.toLowerCase(Locale.ENGLISH).replaceAll("\\s", "_");
+	}
+}
