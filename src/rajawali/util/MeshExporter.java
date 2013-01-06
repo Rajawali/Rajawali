136830b067713dcbd01767c0f79929c15e25efc4
Jay Weisskopf <jay@jayschwa.net>
2013-01-06 11:56:32 -0600

    Auto-format MeshExporter and update messages

--- src/rajawali/util/MeshExporter.java
+++ src/rajawali/util/MeshExporter.java
@@ -17,32 +17,35 @@ import rajawali.animation.mesh.VertexAnimationObject3D;
 import android.os.Environment;
 
 public class MeshExporter {
+
 	private BaseObject3D mObject;
 	private String mFileName;
 	private boolean mCompressed;
 	private File mExportDir = null;
-	
+
 	public enum ExportType {
 		SERIALIZED,
 		OBJ
 	}
-	
+
 	public MeshExporter(BaseObject3D objectToExport) {
 		mObject = objectToExport;
 	}
-	
-	public void setExportDirectory(File exportDir){
+
+	public void setExportDirectory(File exportDir) {
+		// The path's validity is the user's responsibility.
+		// Any problems are taken care of by the try blocks in the private export methods.
 		mExportDir = exportDir;
 	}
-	
+
 	public void export(String fileName, ExportType type) {
 		export(fileName, type, false);
 	}
-	
+
 	public void export(String fileName, ExportType type, boolean compressed) {
 		mFileName = fileName;
 		mCompressed = compressed;
-		switch(type) {
+		switch (type) {
 		case SERIALIZED:
 			exportToSerialized();
 			break;
@@ -51,74 +54,68 @@ public class MeshExporter {
 			break;
 		}
 	}
-	
-	// The path's validity is the user's responsibility.
-	// Any problems are taken care of by the try blocks in the private export methods.
-	
-	private File getExportFile(){
+
+	private File getExportFile() {
 		File path;
-		if(mExportDir == null)
+		if (mExportDir == null)
 			path = Environment.getExternalStorageDirectory();
 		else
 			path = mExportDir;
-		
-		// I don't know why the previous directory construction had to be so complex.
-		// This constructor does the exact same thing, if I'm not mistaken.
 		return new File(path, mFileName);
-		
-		
+
 	}
-	
+
 	private void exportToObj() {
-		RajLog.d("Exporting " +mObject.getName()+ " as .obj file");
+		RajLog.d("Exporting " + mObject.getName() + " as .obj file");
 		Geometry3D g = mObject.getGeometry();
 		StringBuffer sb = new StringBuffer();
-		
+
 		sb.append("# Exported by Rajawali 3D Engine for Android\n");
 		sb.append("o ");
 		sb.append(mObject.getName());
 		sb.append("\n");
-		
-		for(int i=0; i<g.getVertices().capacity(); i+=3) {
+
+		for (int i = 0; i < g.getVertices().capacity(); i += 3) {
 			sb.append("v ");
 			sb.append(g.getVertices().get(i));
 			sb.append(" ");
-			sb.append(g.getVertices().get(i+1));
+			sb.append(g.getVertices().get(i + 1));
 			sb.append(" ");
-			sb.append(g.getVertices().get(i+2));
+			sb.append(g.getVertices().get(i + 2));
 			sb.append("\n");
 		}
-		
+
 		sb.append("\n");
-		
-		for(int i=0; i<g.getTextureCoords().capacity(); i+=2) {
+
+		for (int i = 0; i < g.getTextureCoords().capacity(); i += 2) {
 			sb.append("vt ");
 			sb.append(g.getTextureCoords().get(i));
 			sb.append(" ");
-			sb.append(g.getTextureCoords().get(i+1));
+			sb.append(g.getTextureCoords().get(i + 1));
 			sb.append("\n");
 		}
-		
+
 		sb.append("\n");
-		
-		for(int i=0; i<g.getNormals().capacity(); i+=3) {
+
+		for (int i = 0; i < g.getNormals().capacity(); i += 3) {
 			sb.append("vn ");
 			sb.append(g.getNormals().get(i));
 			sb.append(" ");
-			sb.append(g.getNormals().get(i+1));
+			sb.append(g.getNormals().get(i + 1));
 			sb.append(" ");
-			sb.append(g.getNormals().get(i+2));
+			sb.append(g.getNormals().get(i + 2));
 			sb.append("\n");
 		}
-		
+
 		sb.append("\n");
-		
+
 		boolean isIntBuffer = g.getIndices() instanceof IntBuffer;
-		
-		for(int i=0; i<g.getIndices().capacity(); i++) {
-			if(i%3 == 0)
+
+		for (int i = 0; i < g.getIndices().capacity(); i++) {
+			if (i % 3 == 0)
 				sb.append("\nf ");
-			int index = isIntBuffer ? ((IntBuffer)g.getIndices()).get(i) + 1 : ((ShortBuffer)g.getIndices()).get(i) + 1;
+			int index = isIntBuffer ? ((IntBuffer) g.getIndices()).get(i) + 1
+					: ((ShortBuffer) g.getIndices()).get(i) + 1;
 			sb.append(index);
 			sb.append("/");
 			sb.append(index);
@@ -126,36 +123,32 @@ public class MeshExporter {
 			sb.append(index);
 			sb.append(" ");
 		}
-		
-		try
-	    {
-			
+
+		try {
 
 			File f = getExportFile();
-	        FileWriter writer = new FileWriter(f);
-	        writer.append(sb.toString());
-	        writer.flush();
-	        writer.close();
-	        
-	        RajLog.d(".obj export successful: " + sdcardPath + File.separator + mFileName);
-	    }
-	    catch(IOException e)
-	    {
-	         e.printStackTrace();
-	    }
+			FileWriter writer = new FileWriter(f);
+			writer.append(sb.toString());
+			writer.flush();
+			writer.close();
+
+			RajLog.d(".obj export successful: " + f.getCanonicalPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	/**
-	 * Make sure this line is in your AndroidManifer.xml file, under <manifest>:
-	 * <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+	 * Make sure this line is in your AndroidManifer.xml file, under <manifest>: <uses-permission
+	 * android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
 	 */
 	private void exportToSerialized() {
 		FileOutputStream fos;
-		try {			
+		try {
 			File f = getExportFile();
 			fos = new FileOutputStream(f);
 			ObjectOutputStream os = null;
-			if(mCompressed) {
+			if (mCompressed) {
 				GZIPOutputStream gz = new GZIPOutputStream(fos);
 				os = new ObjectOutputStream(gz);
 			} else {
@@ -163,16 +156,16 @@ public class MeshExporter {
 			}
 
 			SerializedObject3D ser = mObject.toSerializedObject3D();
-			
-			if(mObject instanceof VertexAnimationObject3D) {
-				VertexAnimationObject3D o = (VertexAnimationObject3D)mObject;
+
+			if (mObject instanceof VertexAnimationObject3D) {
+				VertexAnimationObject3D o = (VertexAnimationObject3D) mObject;
 				int numFrames = o.getNumFrames();
 				float[][] vs = new float[numFrames][];
 				float[][] ns = new float[numFrames][];
 				String[] frameNames = new String[numFrames];
-				
-				for( int i=0; i<numFrames; ++i) {
-					VertexAnimationFrame frame = (VertexAnimationFrame)o.getFrame(i);
+
+				for (int i = 0; i < numFrames; ++i) {
+					VertexAnimationFrame frame = (VertexAnimationFrame) o.getFrame(i);
 					Geometry3D geom = frame.getGeometry();
 					float[] v = new float[geom.getVertices().limit()];
 					geom.getVertices().get(v);
@@ -182,17 +175,17 @@ public class MeshExporter {
 					ns[i] = n;
 					frameNames[i] = frame.getName();
 				}
-				
+
 				ser.setFrameVertices(vs);
 				ser.setFrameNormals(ns);
 				ser.setFrameNames(frameNames);
 			}
-			
+
 			os.writeObject(ser);
 			os.close();
-			RajLog.i("Successfully serialized " + mFileName + " to SD card.");
+			RajLog.i("Successfully serialized " + mFileName);
 		} catch (Exception e) {
-			RajLog.e("Serializing " + mFileName + " to SD card was unsuccessfull.");
+			RajLog.e("Serializing " + mFileName + " was unsuccessfull.");
 			e.printStackTrace();
 		}
 
