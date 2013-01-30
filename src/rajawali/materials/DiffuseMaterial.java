2affa195941305f016d39cf83e11025dcebc56c8
David Trounstine <david@evvid.com>
2013-01-29 21:25:36 -0800

    Added lights to diffuse

--- src/rajawali/materials/DiffuseMaterial.java
+++ src/rajawali/materials/DiffuseMaterial.java
@@ -67,7 +67,7 @@ public class DiffuseMaterial extends AAdvancedMaterial {
 		"%LIGHT_VARS%" +
 		
 		"void main() {\n" +
-		"	float intensity = 0.0;\n" +
+		"	vec3 Kd = vec3(0.0);\n" +
 		"	float dist = 0.0;\n" +
 		"	vec3 L = vec3(0.0);\n" +
 		"#ifdef TEXTURED\n" +
@@ -78,7 +78,7 @@ public class DiffuseMaterial extends AAdvancedMaterial {
 
 	    "%LIGHT_CODE%" +
 
-		"	gl_FragColor.rgb = uAmbientIntensity.rgb * uAmbientColor.rgb + intensity * gl_FragColor.rgb;\n" +
+		"	gl_FragColor.rgb = uAmbientIntensity.rgb * uAmbientColor.rgb + Kd * gl_FragColor.rgb;\n" +
 		M_FOG_FRAGMENT_COLOR +		
 		"}";
 	
@@ -117,8 +117,7 @@ public class DiffuseMaterial extends AAdvancedMaterial {
 				vc.append("vAttenuation").append(i).append(" = 1.0;\n");
 				sb.append("L = -normalize(uLightDirection").append(i).append(");\n");				
 			}
-//			sb.append("gl_FragColor.rgb += uLightColor").append(i).append(";\n");
-			sb.append("intensity += uLightPower").append(i).append(" * max(dot(N, L), 0.1) * vAttenuation").append(i).append(";\n");
+			sb.append("Kd += uLightColor").append(i).append(" * (uLightPower").append(i).append(" * max(dot(N, L), 0.1) * vAttenuation").append(i).append(");\n");
 		}
 		
 		super.setShaders(vertexShader.replace("%LIGHT_CODE%", vc.toString()), fragmentShader.replace("%LIGHT_CODE%", sb.toString()));
