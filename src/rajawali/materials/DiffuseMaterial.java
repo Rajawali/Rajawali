6344bfafa936baeea2c0f718201bd79ef7485a7d
David Trounstine <david@evvid.com>
2013-02-01 11:10:51 -0800

    Reduced number of calculation

--- src/rajawali/materials/DiffuseMaterial.java
+++ src/rajawali/materials/DiffuseMaterial.java
@@ -105,24 +105,26 @@ public class DiffuseMaterial extends AAdvancedMaterial {
 	}
 	
 	public void setShaders(String vertexShader, String fragmentShader) {
-		StringBuffer sb = new StringBuffer();
+		StringBuffer fc = new StringBuffer();
 		StringBuffer vc = new StringBuffer();
-		
+		fc.append("float normPower = 0.0");
+
 		for(int i=0; i<mLights.size(); ++i) {
 			ALight light = mLights.get(i);
 			
 			if(light.getLightType() == ALight.POINT_LIGHT) {
-				sb.append("L = normalize(uLightPosition").append(i).append(" - V.xyz);\n");
+				fc.append("L = normalize(uLightPosition").append(i).append(" - V.xyz);\n");
 				vc.append("dist = distance(V.xyz, uLightPosition").append(i).append(");\n");
 				vc.append("vAttenuation").append(i).append(" = 1.0 / (uLightAttenuation").append(i).append("[1] + uLightAttenuation").append(i).append("[2] * dist + uLightAttenuation").append(i).append("[3] * dist * dist);\n");
 			} else if(light.getLightType() == ALight.DIRECTIONAL_LIGHT) {
 				vc.append("vAttenuation").append(i).append(" = 1.0;\n");
-				sb.append("L = -normalize(uLightDirection").append(i).append(");\n");				
+				fc.append("L = -normalize(uLightDirection").append(i).append(");\n");				
 			}
-			sb.append("intensity +=  uLightPower").append(i).append(" * max(dot(N, L), 0.1) * vAttenuation").append(i).append(";\n");
-			sb.append("Kd += uLightColor").append(i).append(" * uLightPower").append(i).append(" * max(dot(N, L), 0.1) * vAttenuation").append(i).append(";\n");
+			fc.append("normPower =  uLightPower").append(i).append(" * max(dot(N, L), 0.1) * vAttenuation").append(i).append(";\n");
+			fc.append("intensity +=  normPower\n");
+			fc.append("Kd += uLightColor").append(i).append(" * normPower;\n");
 		}
 		
-		super.setShaders(vertexShader.replace("%LIGHT_CODE%", vc.toString()), fragmentShader.replace("%LIGHT_CODE%", sb.toString()));
+		super.setShaders(vertexShader.replace("%LIGHT_CODE%", vc.toString()), fragmentShader.replace("%LIGHT_CODE%", fc.toString()));
 	}
 }
