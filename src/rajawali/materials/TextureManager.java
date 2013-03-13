52d5e12263fdebf8b289cd97a8808e6bdc56cf48
Andrew Jo <andrewjo@gmail.com>
2013-03-14 00:43:23 +0900

    Fixed a typo in the mipmap chaining code

--- src/rajawali/materials/TextureManager.java
+++ src/rajawali/materials/TextureManager.java
@@ -279,7 +279,7 @@ public class TextureManager {
 		        		for (int i = 0; i < buffer.length; i++) {
 		        			GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, i, bitmapFormat, w, h, 0, bitmapFormat, GLES20.GL_UNSIGNED_BYTE, buffer[i]);
 		        			w = w > 1 ? w / 2 : 1;
-		        			h = h > 1 ? w / 2 : 1;
+		        			h = h > 1 ? h / 2 : 1;
 		        		}
 	        		}
 	        	} else { 
@@ -290,7 +290,7 @@ public class TextureManager {
 		        		for (int i = 0; i < buffer.length; i++) {
 	        				GLES20.glCompressedTexImage2D(GLES20.GL_TEXTURE_2D, i, compressionFormat, w, h, 0, buffer[i].capacity(), buffer[i]);
 	        				w = w > 1 ? w / 2 : 1;
-	        				h = h > 1 ? w / 2 : 1;
+	        				h = h > 1 ? h / 2 : 1;
 		        		}
 	        		}
 	        	}
