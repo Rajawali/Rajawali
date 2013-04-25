8c7fdfab43c58b27d7912bb43a96c90253d3f4f8
Ian thomas <ian.shaun.thomas@gmail.com>
2013-04-24 21:10:11 -0400

    #578 Added Animation3DQueue

--- src/rajawali/animation/Animation3DQueue.java
+++ src/rajawali/animation/Animation3DQueue.java
@@ -1,5 +1,56 @@
 package rajawali.animation;
 
-public class Animation3DQueue {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Animation3DQueue implements IAnimation3DListener {
+
+	private final List<Animation3D> mAnimations;
+	
+	private IAnimation3DListener mAnimationListener;
+	private int mCurrentAnimation;
+
+	public Animation3DQueue() {
+		mAnimations = new ArrayList<Animation3D>();
+		mCurrentAnimation = 0;
+	}
+
+	public void addAnimation(Animation3D animation) {
+		mAnimations.add(animation);
+		animation.registerListener(this);
+	}
+
+	public void setAnimationListener(IAnimation3DListener animationListener) {
+		mAnimationListener = animationListener;
+	}
+
+	public void onAnimationEnd(Animation3D animation) {
+		if (mCurrentAnimation == mAnimations.size() - 1) {
+			if (mAnimationListener != null)
+				mAnimationListener.onAnimationEnd(null);
+			
+			mCurrentAnimation = 0;
+			return;
+		}
+		mAnimations.get(++mCurrentAnimation).play();
+	}
+
+	public void onAnimationRepeat(Animation3D animation) {}
+
+	public void onAnimationStart(Animation3D animation) {
+
+	}
+
+	public void onAnimationUpdate(Animation3D animation, double interpolatedTime) {
+
+	}
+
+	public void start() {
+		if (mAnimations.size() == 0)
+			return;
+		
+		mAnimations.get(0).play();
+		if (mAnimationListener != null)
+			mAnimationListener.onAnimationStart(null);
+	}
 }
