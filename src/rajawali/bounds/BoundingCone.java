c1f2a9f7710682f1f701be114d04c70ee2aa085e
Jared Woolston <jwoolston@tenkiv.com>
2013-05-06 20:04:48 -0700

    Adding forgotten BoundingCone.
    Signed-off-by: Jared Woolston <jwoolston@tenkiv.com>

---
+++ src/rajawali/bounds/BoundingCone.java
@@ -0,0 +1,49 @@
+package rajawali.bounds;
+
+import rajawali.BaseObject3D;
+import rajawali.Camera;
+import rajawali.Geometry3D;
+
+
+public class BoundingCone implements IBoundingVolume {
+
+	public void calculateBounds(Geometry3D geometry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void drawBoundingVolume(Camera camera, float[] projMatrix, float[] vMatrix, float[] mMatrix) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void transform(float[] matrix) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean intersectsWith(IBoundingVolume boundingVolume) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public BaseObject3D getVisual() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setBoundingColor(int color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getBoundingColor() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public VOLUME_SHAPE getVolumeShape() {
+		return VOLUME_SHAPE.CONE;
+	}
+
+}
