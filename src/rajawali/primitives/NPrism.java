7c4bda05f05dc70cefb314e43f405480f10ca730
Jared Woolston <jwoolston@tenkiv.com>
2013-05-11 13:25:38 -0700

    Added eccentricity option to NPrism.
    Signed-off-by: Jared Woolston <jwoolston@tenkiv.com>

--- src/rajawali/primitives/NPrism.java
+++ src/rajawali/primitives/NPrism.java
@@ -15,12 +15,14 @@ import rajawali.math.Number3D;
  */
 public class NPrism extends BaseObject3D {
 
-	protected int mSideCount = 3;
+	protected int mSideCount;
 	protected double mRadiusBase;
 	protected double mRadiusTop;
+	protected double mMinorBase;
+	protected double mMinorTop;
 	protected double mHeight;
-	protected double mFrustumHeight;
-
+	protected double mEccentricity;
+	
 	private static final Number3D UP = new Number3D(0, 1, 0);
 	private static final Number3D DOWN = new Number3D(0, -1, 0);
 
@@ -34,7 +36,7 @@ public class NPrism extends BaseObject3D {
 	public NPrism(int sides, double radius, double height) {
 		this(sides, 0, radius, height);
 	}
-
+	
 	/**
 	 * Creates a frustum like prism.
 	 * 
@@ -44,12 +46,35 @@ public class NPrism extends BaseObject3D {
 	 * @param height Double the height of the prism.
 	 */
 	public NPrism(int sides, double radiusTop, double radiusBase, double height) {
+		this(sides, radiusTop, radiusBase, 0.0, height);
+	}
+	
+	/**
+	 * Creates a frustum like prism with elliptical base rather than circular. 
+	 * The major axis is equivalent to the radius specified and the minor axis
+	 * is computed from the eccentricity.
+	 * 
+	 * @param sides Integer number of sides to the prism.
+	 * @param radiusTop Double the radius of the top.
+	 * @param radiusBase Double the radius of the base.
+	 * @param eccentricity Double the eccentricity of the ellipse.
+	 * @param height Double the height of the prism.
+	 */
+	public NPrism(int sides, double radiusTop, double radiusBase, double eccentricity, double height) {
 		if (sides < 3) throw new IllegalArgumentException("Prisms must have at least 3 sides!");
+		if ((eccentricity < 0) || (eccentricity >= 1)) throw new IllegalArgumentException("Eccentricity must be in the range [0,1)");
+		mSideCount = sides;
+		mEccentricity = eccentricity;
 		mRadiusTop = radiusTop;
+		mMinorTop = calculateMinorAxis(mRadiusTop);
 		mRadiusBase = radiusBase;
-		mFrustumHeight = height;
+		mMinorBase = calculateMinorAxis(mRadiusBase);
 		mHeight = height;
 		init();
+	}
+	
+	protected double calculateMinorAxis(double major) {
+		return Math.sqrt(Math.pow(major, 2.0)*(1 - Math.pow(mEccentricity, 2.0)));
 	}
 
 	protected void init() {
@@ -72,8 +97,7 @@ public class NPrism extends BaseObject3D {
 		double x = 1.0f, y = 1.0f, z = 1.0f;
 		double u = 0, v = 1;
 		double u_delta = 1.0/mSideCount;
-		double h = mHeight;
-		double MAG = Math.sqrt(Math.pow((mRadiusTop - mRadiusBase), 2.0) + Math.pow(h, 2.0));
+		double MAG = Math.sqrt(Math.pow((mRadiusTop - mRadiusBase), 2.0) + Math.pow(mHeight, 2.0));
 		Number3D temp_normal = new Number3D();
 		if (mSideCount % 2 == 0) angle = angle_delta/2.0;
 
@@ -82,15 +106,15 @@ public class NPrism extends BaseObject3D {
 		angle = (mSideCount % 2 == 0) ? angle = angle_delta/2.0 : 0;
 		
 		x = mRadiusTop*Math.cos(angle);
-		z = mRadiusTop*Math.sin(angle);
+		z = mMinorTop*Math.sin(angle);
 		for (int side = 0; side < mSideCount; ++side) {
 			base_index = 3*triangle;
 			//Handle the top
 			y = mHeight/2;
 			v = 0;
-			temp_normal.x = (float) (h*Math.cos(angle + angle_delta/2)/MAG);
-			temp_normal.y = (float) (h/MAG);
-			temp_normal.z = (float) (h*Math.sin(angle + angle_delta/2)/MAG);
+			temp_normal.x = (float) (mMinorTop*MAG*Math.cos(angle + angle_delta/2));
+			temp_normal.y = (float) MAG;
+			temp_normal.z = (float) (mRadiusTop*MAG*Math.sin(angle + angle_delta/2));
 			temp_normal.normalize();
 			temp_normal.z = -temp_normal.z;
 
@@ -107,7 +131,7 @@ public class NPrism extends BaseObject3D {
 			y = -mHeight/2;
 			v = 1;
 			x = mRadiusBase*Math.cos(angle);
-			z = mRadiusBase*Math.sin(angle);
+			z = mMinorBase*Math.sin(angle);
 			vertices[offset] = (float) x;
 			texture[2*vertex] = (float) u;
 			texture[2*vertex+1] = (float) v;
@@ -121,7 +145,7 @@ public class NPrism extends BaseObject3D {
 			angle += angle_delta;
 			u += u_delta;
 			x = mRadiusBase*Math.cos(angle);
-			z = mRadiusBase*Math.sin(angle);
+			z = mMinorBase*Math.sin(angle);
 			vertices[offset] = (float) x;
 			texture[2*vertex] = (float) u;
 			texture[2*vertex+1] = (float) v;
@@ -136,7 +160,7 @@ public class NPrism extends BaseObject3D {
 			y = mHeight/2;
 			v = 0;
 			x = mRadiusTop*Math.cos(angle);
-			z = mRadiusTop*Math.sin(angle);
+			z = mMinorTop*Math.sin(angle);
 			vertices[offset] = (float) x;
 			texture[2*vertex] = (float) u;
 			texture[2*vertex+1] = (float) v;
@@ -175,13 +199,14 @@ public class NPrism extends BaseObject3D {
 
 		offset = offset_holder;
 		angle = (mSideCount % 2 == 0) ? angle = angle_delta/2.0 : 0;
+		double minorTexture = calculateMinorAxis(1.0);
 		y = mHeight/2;
 		for (int side = 0; side < mSideCount; ++side) {
 			base_index = 3*triangle;
 			x = mRadiusTop*Math.cos(angle);
-			z = mRadiusTop*Math.sin(angle);
+			z = mMinorTop*Math.sin(angle);
 			u = Math.cos(angle);
-			v = Math.sin(angle);
+			v = minorTexture*Math.sin(angle);
 			//Handle the top
 			vertices[offset] = (float) x;
 			texture[2*vertex] = (float) u;
@@ -208,9 +233,9 @@ public class NPrism extends BaseObject3D {
 		for (int side = 0; side < mSideCount; ++side) {
 			base_index = 3*triangle;
 			x = mRadiusBase*Math.cos(angle);
-			z = mRadiusBase*Math.sin(angle);
-			u = -Math.cos(angle);
-			v = Math.sin(angle);
+			z = mMinorBase*Math.sin(angle);
+			u = Math.cos(angle);
+			v = -minorTexture*Math.sin(angle);
 			//Handle the bottom
 			vertices[offset] = (float) x;
 			texture[2*vertex] = (float) u;
