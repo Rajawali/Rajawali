7798849e21c0e6695bb60491848e5b96a75d2cac
MasDennis <ippeldv@gmail.com>
2013-06-25 10:53:31 +0100

    TranslateAnimation orient to path fix. Cleaned up the calculations and started using Quaternion.setFromAngleBetween(). This takes the current object's orientation and the current tangent vector. This fixes issue #863.

--- src/rajawali/ATransformable3D.java
+++ src/rajawali/ATransformable3D.java
@@ -121,6 +121,8 @@ public abstract class ATransformable3D extends AFrameTask implements IGraphNodeM
 			mLookAtMatrix[8] = mTmpRotZ.x;
 			mLookAtMatrix[9] = mTmpRotZ.y;
 			mLookAtMatrix[10] = mTmpRotZ.z;
+			
+			mOrientation.fromRotationMatrix(mLookAtMatrix);
 		} else {
 			mOrientation.multiply(mTmpOrientation.fromAngleAxis(mIsCamera ? mRotation.y : mRotation.y, mAxisY));
 			mOrientation.multiply(mTmpOrientation.fromAngleAxis(mIsCamera ? mRotation.z : mRotation.z, mAxisZ));
