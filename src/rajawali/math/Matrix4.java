820d528ddf28bef80687f9edc3e9708dffe747e6
Jared Woolston <jwoolston@tenkiv.com>
2013-07-23 08:32:16 -0700

    Fixes #967. There was a bug in the Matrix4#transpose() method which used the wrong indices. Thanks @kostik1337 for reporting this.
    Signed-off-by: Jared Woolston <jwoolston@tenkiv.com>

--- src/rajawali/math/Matrix4.java
+++ src/rajawali/math/Matrix4.java
@@ -304,9 +304,9 @@ public final class Matrix4 {
 	public Matrix4 transpose()
     {
         return new Matrix4(m[0], m[4], m[8], m[12],
-                       m[1], m[5], m[8], m[13],
-                       m[2], m[6], m[9], m[14],
-                       m[3], m[7], m[10], m[15]);
+                       m[1], m[5], m[9], m[13],
+                       m[2], m[6], m[10], m[14],
+                       m[3], m[7], m[11], m[15]);
     }
 	
 	public void setTranslation(final Vector3 v )
