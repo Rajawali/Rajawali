0d707895071e43e9d5dc71fe47b30b8b68bc8ad5
Ravenfeld <alexis.lecanu@gmail.com>
2013-09-13 17:58:22 +0200

    Fix bug rotation camera in offset
    Réfléchir si c est pas mieux de déplacer la camera et non les éléments.
    par example dire a la camera le déplacement max en fonction du
    background, y réfléchir.
    Pour l'instant correction faite mais pas définitive, car pas bonne a
    mon avis

--- src/rajawali/primitives/PointSprite.java
+++ src/rajawali/primitives/PointSprite.java
@@ -1,20 +1,24 @@
 package rajawali.primitives;
 
 import rajawali.Camera;
+import rajawali.Camera2D;
 import rajawali.math.Matrix4;
-import rajawali.math.vector.Vector3.Axis;
 import rajawali.util.ObjectColorPicker.ColorPickerInfo;
 
 
 public class PointSprite extends Plane {
 	public PointSprite(float width, float height) {
-		super(width, height, 1, 1, Axis.Z);
+		super(width, height, 1, 1);
 	}
 	
 	@Override
 	public void render(Camera camera, final Matrix4 vpMatrix, final Matrix4 projMatrix, final Matrix4 vMatrix, 
 			final Matrix4 parentMatrix, ColorPickerInfo pickerInfo) {
-		setLookAt(camera.getPosition());		
+		if (!(camera instanceof Camera2D)) {
+			setLookAt(camera.getPosition());
+		} else {
+			setRotY(180);
+		}
 		super.render(camera, vpMatrix, projMatrix, vMatrix, parentMatrix, pickerInfo);
 	}
 }
