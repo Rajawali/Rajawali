cb20b9635c2979fa5dcd50b0a2868226e135f9c1
Alexis Lecanu <alexis.lecanu@gmail.com>
2013-09-14 08:52:32 +0200

    Revert "Fix bug rotation camera in offset"
    This reverts commit 0d707895071e43e9d5dc71fe47b30b8b68bc8ad5.

--- src/rajawali/primitives/PointSprite.java
+++ src/rajawali/primitives/PointSprite.java
@@ -1,24 +1,20 @@
 package rajawali.primitives;
 
 import rajawali.Camera;
-import rajawali.Camera2D;
 import rajawali.math.Matrix4;
+import rajawali.math.vector.Vector3.Axis;
 import rajawali.util.ObjectColorPicker.ColorPickerInfo;
 
 
 public class PointSprite extends Plane {
 	public PointSprite(float width, float height) {
-		super(width, height, 1, 1);
+		super(width, height, 1, 1, Axis.Z);
 	}
 	
 	@Override
 	public void render(Camera camera, final Matrix4 vpMatrix, final Matrix4 projMatrix, final Matrix4 vMatrix, 
 			final Matrix4 parentMatrix, ColorPickerInfo pickerInfo) {
-		if (!(camera instanceof Camera2D)) {
-			setLookAt(camera.getPosition());
-		} else {
-			setRotY(180);
-		}
+		setLookAt(camera.getPosition());		
 		super.render(camera, vpMatrix, projMatrix, vMatrix, parentMatrix, pickerInfo);
 	}
 }
