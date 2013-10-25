9a52ab04ad5b6bc0df4ea7022e3a9431f418f3d7
Ian Thomas <toxicbakery@gmail.com>
2013-10-24 20:43:26 -0400

    - Fixed bug in renderer causing object picking to only select the first picked object. - Fixed bug in RenderTargetTexture causing NPE. - Optimized the renderer code for picking such that the color picking material is only referenced once now and the render has one less else clause now. - Cleaned up ObjectColorPicker quite a bit and changed the circular calls to a static call for readability: pickerInfo.getPicker().createColorPickingTexture(pickerInfo); Is now: ObjectColorPicker.createColorPickingTexture(pickerInfo);

--- src/rajawali/util/ObjectColorPicker.java
+++ src/rajawali/util/ObjectColorPicker.java
@@ -27,9 +27,10 @@ import android.opengl.GLES20;
 
 public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 
-	private ArrayList<Object3D> mObjectLookup;
+	private final ArrayList<Object3D> mObjectLookup = new ArrayList<Object3D>();
+	private final RajawaliRenderer mRenderer;
+
 	private int mColorIndex = 0;
-	private RajawaliRenderer mRenderer;
 	private int mFrameBufferHandle = -1;
 	private int mDepthBufferHandle = -1;
 	private RenderTargetTexture mTexture;
@@ -38,13 +39,12 @@ public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 	private OnObjectPickedListener mObjectPickedListener;
 
 	public ObjectColorPicker(RajawaliRenderer renderer) {
-		mObjectLookup = new ArrayList<Object3D>();
 		mRenderer = renderer;
 		mRenderer.queueInitializeTask(this);
 	}
 
 	public void initialize() {
-		int size = Math.max(mRenderer.getViewportWidth(), mRenderer.getViewportHeight());
+		final int size = Math.max(mRenderer.getViewportWidth(), mRenderer.getViewportHeight());
 		mTexture = new RenderTargetTexture("colorPickerTexture");
 		mTexture.setWidth(size);
 		mTexture.setHeight(size);
@@ -60,15 +60,16 @@ public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 	public void reload() {
 		if (!mIsInitialized)
 			return;
+
 		genBuffers();
 	}
 
 	public void genBuffers() {
-		int[] frameBuffers = new int[1];
+		final int[] frameBuffers = new int[1];
 		GLES20.glGenFramebuffers(1, frameBuffers, 0);
 		mFrameBufferHandle = frameBuffers[0];
 
-		int[] depthBuffers = new int[1];
+		final int[] depthBuffers = new int[1];
 		GLES20.glGenRenderbuffers(1, depthBuffers, 0);
 		mDepthBufferHandle = depthBuffers[0];
 
@@ -109,7 +110,7 @@ public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 		if (!mObjectLookup.contains(object)) {
 			mObjectLookup.add(object);
 			object.setPickingColor(mColorIndex);
-			mColorIndex++;
+			++mColorIndex;
 		}
 	}
 
@@ -123,24 +124,25 @@ public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 		mRenderer.getCurrentScene().requestColorPickingTexture(new ColorPickerInfo(x, y, this));
 	}
 
-	public void createColorPickingTexture(ColorPickerInfo pickerInfo) {
-		ByteBuffer pixelBuffer = ByteBuffer.allocateDirect(4);
+	public static void createColorPickingTexture(ColorPickerInfo pickerInfo) {
+		final ObjectColorPicker picker = pickerInfo.getPicker();
+		final ByteBuffer pixelBuffer = ByteBuffer.allocateDirect(4);
 		pixelBuffer.order(ByteOrder.nativeOrder());
-		GLES20.glReadPixels((int) pickerInfo.getX(), mRenderer.getViewportHeight()
-				- (int) pickerInfo.getY(), 1, 1, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE,
+
+		GLES20.glReadPixels(pickerInfo.getX(), picker.mRenderer.getViewportHeight()
+				- pickerInfo.getY(), 1, 1, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE,
 				pixelBuffer);
 		GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
 		pixelBuffer.rewind();
 
-		int r = pixelBuffer.get(0) & 0xff;
-		int g = pixelBuffer.get(1) & 0xff;
-		int b = pixelBuffer.get(2) & 0xff;
-		int a = pixelBuffer.get(3) & 0xff;
-		int index = Color.argb(a, r, g, b);
+		final int r = pixelBuffer.get(0) & 0xff;
+		final int g = pixelBuffer.get(1) & 0xff;
+		final int b = pixelBuffer.get(2) & 0xff;
+		final int a = pixelBuffer.get(3) & 0xff;
+		final int index = Color.argb(a, r, g, b);
 
-		if (0 <= index && index < mObjectLookup.size() && mObjectPickedListener != null) {
-			mObjectPickedListener.onObjectPicked(mObjectLookup.get(index));
-		}
+		if (0 <= index && index < picker.mObjectLookup.size() && picker.mObjectPickedListener != null)
+			picker.mObjectPickedListener.onObjectPicked(picker.mObjectLookup.get(index));
 	}
 
 	public Material getMaterial() {
@@ -149,14 +151,14 @@ public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 
 	public class ColorPickerInfo {
 
-		private float mX;
-		private float mY;
+		private int mX;
+		private int mY;
 		private ObjectColorPicker mPicker;
 		private ByteBuffer mColorPickerBuffer;
 
 		public ColorPickerInfo(float x, float y, ObjectColorPicker picker) {
-			mX = x;
-			mY = y;
+			mX = (int) x;
+			mY = (int) y;
 			mPicker = picker;
 		}
 
@@ -164,11 +166,11 @@ public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 			return mPicker;
 		}
 
-		public float getX() {
+		public int getX() {
 			return mX;
 		}
 
-		public float getY() {
+		public int getY() {
 			return mY;
 		}
 
@@ -181,7 +183,12 @@ public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 		}
 	}
 
-	public static class ObjectColorPickerException extends Exception {
+	@Override
+	public TYPE getFrameTaskType() {
+		return TYPE.COLOR_PICKER;
+	}
+
+	public static final class ObjectColorPickerException extends Exception {
 
 		private static final long serialVersionUID = 3732833696361901287L;
 
@@ -201,10 +208,5 @@ public class ObjectColorPicker extends AFrameTask implements IObjectPicker {
 			super(msg, throwable);
 		}
 
-	}
-
-	@Override
-	public TYPE getFrameTaskType() {
-		return TYPE.COLOR_PICKER;
 	}
 }
