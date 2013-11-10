f222f2486b243c0fe9326a0efac83401a4c51733
Alexis Lecanu <alexis.lecanu@gmail.com>
2013-11-10 17:11:51 +0100

    Add direction for scrolling

--- src/rajawali/animation/ScrollingAnimationTexture.java
+++ src/rajawali/animation/ScrollingAnimationTexture.java
@@ -4,13 +4,27 @@ import rajawali.materials.textures.ATexture;
 
 public class ScrollingAnimationTexture extends AnimationTexture {
 
+    public enum DirectionMode {
+        RIGHT_TO_LEFT,LEFT_TO_RIGHT;
+    }
+
+    protected DirectionMode mDirection = DirectionMode.RIGHT_TO_LEFT;
+
     public ScrollingAnimationTexture() {
         super();
     }
 
     @Override
     protected void applyTransformation() {
-        mTexture.setOffsetU((float) mInterpolatedTime);
+        switch (mDirection){
+            case RIGHT_TO_LEFT:
+                mTexture.setOffsetU((float) mInterpolatedTime);
+                break;
+            case LEFT_TO_RIGHT:
+                mTexture.setOffsetU(1.0f - (float) mInterpolatedTime);
+                break;
+        }
+
     }
 
     public void setTexture(ATexture texture) {
@@ -18,5 +32,7 @@ public class ScrollingAnimationTexture extends AnimationTexture {
         mTexture.enableOffset(true);
         mTexture.setOffsetU(0f);
     }
+
+    public void setDirectionMode(DirectionMode direction){ mDirection = direction;}
 }
 
