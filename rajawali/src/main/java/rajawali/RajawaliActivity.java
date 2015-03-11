2be0a65b821593c905aa53bd2deae2de5602cdc0
Ian Thomas <toxicbakery@gmail.com>
2015-03-11 19:22:55 -0400

    #1366 Extracted EGL Config to its own class and updated RajawaliActivity.java, RajawaliDaydream.java, and RajawaliFragment.java implementations.

--- rajawali/src/main/java/rajawali/RajawaliActivity.java
+++ rajawali/src/main/java/rajawali/RajawaliActivity.java
@@ -12,12 +12,9 @@
  */
 package rajawali;
 
-import javax.microedition.khronos.egl.EGL10;
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.egl.EGLDisplay;
-
 import rajawali.renderer.RajawaliRenderer;
-import rajawali.util.RajLog;
+import rajawali.util.egl.RajawaliEGLConfigChooser;
+
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
@@ -42,7 +39,6 @@ public class RajawaliActivity extends Activity {
 	protected GLSurfaceView mSurfaceView;
 	protected FrameLayout mLayout;
 	protected boolean mMultisamplingEnabled = false;
-	protected boolean mUsesCoverageAa;
 	private RajawaliRenderer mRajRenderer;
 	protected boolean checkOpenGLVersion = true;
 	protected boolean mDeferGLSurfaceViewCreation = false;
@@ -76,88 +72,7 @@ public class RajawaliActivity extends Activity {
     }
     
     protected void createMultisampleConfig() {
-    	final int EGL_COVERAGE_BUFFERS_NV = 0x30E0;
-    	final int EGL_COVERAGE_SAMPLES_NV = 0x30E1;
-    	
-        mSurfaceView.setEGLConfigChooser(new GLSurfaceView.EGLConfigChooser() {
-			public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display) {
-				int[] configSpec = new int[] { 
-						EGL10.EGL_RED_SIZE, 5,
-						EGL10.EGL_GREEN_SIZE, 6,
-						EGL10.EGL_BLUE_SIZE, 5,
-						EGL10.EGL_DEPTH_SIZE, 16,
-						EGL10.EGL_RENDERABLE_TYPE, 4,
-						EGL10.EGL_SAMPLE_BUFFERS, 1,
-						EGL10.EGL_SAMPLES, 2,
-						EGL10.EGL_NONE
-				};
-
-				int[] result = new int[1];
-				if(!egl.eglChooseConfig(display, configSpec, null, 0, result)) {
-					RajLog.e("Multisampling configuration 1 failed.");
-				}
-				
-				if(result[0] <= 0) {
-					// no multisampling, check for coverage multisampling
-					configSpec = new int[] {
-						EGL10.EGL_RED_SIZE, 5,
-						EGL10.EGL_GREEN_SIZE, 6,
-						EGL10.EGL_BLUE_SIZE, 5,
-						EGL10.EGL_DEPTH_SIZE, 16,
-						EGL10.EGL_RENDERABLE_TYPE, 4,
-						EGL_COVERAGE_BUFFERS_NV, 1,
-						EGL_COVERAGE_SAMPLES_NV, 2,
-						EGL10.EGL_NONE
-					};
-					
-					if(!egl.eglChooseConfig(display, configSpec, null, 0, result)) {
-						RajLog.e("Multisampling configuration 2 failed. Multisampling is not possible on your device.");
-					}
-					
-					if(result[0] <= 0) {
-						configSpec = new int[] {
-							EGL10.EGL_RED_SIZE, 5,
-							EGL10.EGL_GREEN_SIZE, 6, 
-							EGL10.EGL_BLUE_SIZE, 5,
-							EGL10.EGL_DEPTH_SIZE, 16,
-							EGL10.EGL_RENDERABLE_TYPE, 4,
-							EGL10.EGL_NONE
-						};
-
-						if(!egl.eglChooseConfig(display, configSpec, null, 0, result)) {
-							RajLog.e("Multisampling configuration 3 failed. Multisampling is not possible on your device.");
-						}
-
-						if(result[0] <= 0) {
-							throw new RuntimeException("Couldn't create OpenGL config.");
-						}
-					} else {
-						mUsesCoverageAa = true;
-					}
-				}
-				EGLConfig[] configs = new EGLConfig[result[0]];
-				if(!egl.eglChooseConfig(display, configSpec, configs, result[0], result)) {
-					throw new RuntimeException("Couldn't create OpenGL config.");
-				}
-				
-				int index = -1;
-				int[] value = new int[1];
-				for(int i=0; i<configs.length; ++i) {
-					egl.eglGetConfigAttrib(display, configs[i], EGL10.EGL_RED_SIZE, value);
-					if(value[0] == 5) {
-						index = i;
-						break;
-					}
-				}
-
-				EGLConfig config = configs.length > 0 ? configs[index] : null;
-				if(config == null) {
-					throw new RuntimeException("No config chosen");
-				}
-				
-				return config;
-			}
-		});
+        mSurfaceView.setEGLConfigChooser(new RajawaliEGLConfigChooser());
     }
     
     protected void setGLBackgroundTransparent(boolean transparent) {
@@ -231,4 +146,5 @@ public class RajawaliActivity extends Activity {
     {
     	mDeferGLSurfaceViewCreation = defer;
     }
+
 }
