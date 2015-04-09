5f8ac8a2e4f752f8e9e43ffe935fe9bfccb22b3a
Jared Woolston <jwoolston@tenkiv.com>
2015-04-08 22:18:04 -0700

    Re-adds some of what was removed in #1464
    Signed-off-by: Jared Woolston <jwoolston@tenkiv.com>

--- rajawali/src/main/java/org/rajawali3d/util/RajLog.java
+++ rajawali/src/main/java/org/rajawali3d/util/RajLog.java
@@ -12,13 +12,21 @@
  */
 package org.rajawali3d.util;
 
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
 import android.util.Log;
+
+import javax.microedition.khronos.opengles.GL10;
 
 public final class RajLog {
 
     public static final String TAG = "Rajawali";
 
     private static boolean debugEnabled;
+
+    private static GL10 sGL10;
 
     public static void setDebugEnabled(boolean flag) {
         debugEnabled = flag;
@@ -49,4 +57,82 @@ public final class RajLog {
         Log.w(TAG, msg);
     }
 
+    public static void setGL10(GL10 gl) {
+        sGL10 = gl;
+    }
+
+    public static void checkGLError(String message) {
+        int error = sGL10.glGetError();
+        if (error > 0)
+            throw new RuntimeException("OpenGL Error: " + GLU.gluErrorString(error) + " " + error + " | " + message);
+    }
+
+    /**
+     * Outputs System and OpenGL information. This function should be called
+     * from initScene.
+     */
+    public static void systemInformation() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("-=-=-=- Device Information -=-=-=-\n");
+        sb.append("Brand : ").append(android.os.Build.BRAND).append("\n");
+        sb.append("Manufacturer : ").append(android.os.Build.MANUFACTURER).append("\n");
+        sb.append("Model : ").append(android.os.Build.MODEL).append("\n");
+        sb.append("Bootloader : ").append(android.os.Build.BOARD).append("\n");
+        sb.append("CPU ABI : ").append(android.os.Build.CPU_ABI).append("\n");
+        sb.append("CPU ABI 2 : ").append(android.os.Build.CPU_ABI2).append("\n");
+        sb.append("-=-=-=- /Device Information -=-=-=-\n\n");
+
+        sb.append("-=-=-=- OpenGL Information -=-=-=-\n");
+        if (sGL10 != null) {
+            sb.append("Vendor : ").append(sGL10.glGetString(GL10.GL_VENDOR)).append("\n");
+            sb.append("Renderer : ").append(sGL10.glGetString(GL10.GL_RENDERER)).append("\n");
+            sb.append("Version : ").append(sGL10.glGetString(GL10.GL_VERSION)).append("\n");
+
+            String extensions = sGL10.glGetString(GL10.GL_EXTENSIONS);
+            String[] ext = extensions.split(" ");
+            int extLength = ext.length;
+
+            if (extLength > 0) {
+                sb.append("Extensions : ").append(ext[0]).append("\n");
+                for (int i = 1; i < extLength; i++) {
+                    sb.append(" : ").append(ext[i]).append("\n");
+                }
+            }
+        } else {
+            sb.append("OpenGL info : Cannot find OpenGL information. Please call this function from initScene().\n");
+        }
+        sb.append("-=-=-=- /OpenGL Information -=-=-=-\n");
+        sb.append(Capabilities.getInstance().toString());
+
+        RajLog.i(sb.toString());
+    }
+
+    /**
+     * Outputs memory characteristics of the device.
+     * Requires a reference to the current Activity.
+     * Available for API 16+
+     *
+     * @param context An activity {@link Context} reference.
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public static void getDeviceMemoryCharacteristics(Context context) {
+        String memInfo;
+        ActivityManager am;
+
+        ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+
+        am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+
+        memInfo = "-----------------------------------------\n";
+        memInfo += "Standard Heap per Application :  " + am.getMemoryClass() + "mb \n";
+        memInfo += "Large Heap per Application :  " + am.getLargeMemoryClass() + "mb \n";
+
+        am.getMemoryInfo(mi);
+
+        memInfo += "Total Device Memory :  " + Math.round(mi.totalMem / 1024 / 1024) + "mb \n";
+        memInfo += "Approximate Memory Available :  " + Math.round(mi.availMem / 1024 / 1024) + "mb \n";
+        memInfo += "-----------------------------------------\n";
+
+        i(memInfo);
+    }
 }
