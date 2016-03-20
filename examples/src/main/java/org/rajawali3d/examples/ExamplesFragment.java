a6f9cb5f0613a15822c7e7110a282ec61f70a7fb
Ian Thomas <ithomas@idealcorp.com>
2016-03-20 16:44:51 -0400

    #1627 Implemented a simple search - Search is based on String.contains() and lower case input and names. - Category name matches include all tests in the category - Zero length search results in all items returned - Single example result is opened when action done is selected

--- examples/src/main/java/org/rajawali3d/examples/ExamplesFragment.java
+++ examples/src/main/java/org/rajawali3d/examples/ExamplesFragment.java
@@ -5,14 +5,18 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.rajawali3d.examples.data.DataSet;
 import org.rajawali3d.examples.recycler.CategoryAdapter;
 
-public class ExamplesFragment extends Fragment {
+public class ExamplesFragment extends Fragment implements SearchView.OnQueryTextListener {
 
     public static final String TAG = "ExamplesFragment";
 
@@ -21,6 +25,7 @@ public class ExamplesFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
 
         adapter = new CategoryAdapter();
         adapter.setCategories(DataSet.getInstance().getCategories());
@@ -35,6 +40,28 @@ public class ExamplesFragment extends Fragment {
         recyclerView.setAdapter(adapter);
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem menuItemSearch = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItemSearch.getActionView();
+        searchView.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return adapter.filterDone(getActivity());
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        adapter.filter(getActivity(), newText);
+        adapter.notifyDataSetChanged();
+        return true;
     }
 
 }
--- examples/src/main/java/org/rajawali3d/examples/recycler/CategoryAdapter.java
+++ examples/src/main/java/org/rajawali3d/examples/recycler/CategoryAdapter.java
@@ -16,12 +16,16 @@ import org.rajawali3d.examples.data.INamed;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 @SuppressWarnings("unchecked")
 public final class CategoryAdapter extends ReferencedAdapter<INamed, NamedReferenceViewHolder, NamedIndexReference<INamed>> implements NamedReferenceViewHolder.IndexReferenceClickListener {
 
     private static final int INDEX_CATEGORY = 1;
     private static final int INDEX_EXAMPLE = 2;
+
+    private List<Category> categoriesOriginal;
+    private List<Category> categoriesDisplayed;
 
     @Override
     protected void onBindViewHolder(NamedReferenceViewHolder holder, int position, NamedIndexReference<INamed> namedIndexReference) {
@@ -59,8 +63,87 @@ public final class CategoryAdapter extends ReferencedAdapter<INamed, NamedRefere
         }
     }
 
+    @Override
+    public void onReferenceClicked(View view, IndexReference reference) {
+        Class type = reference.getType();
+        if (Example.class.equals(type)) {
+            Example example = (Example) reference.get();
+            Context context = view.getContext();
+            onExampleSelected(context, example);
+        } else {
+            throw new IllegalStateException("Unhandled reference for " + type);
+        }
+    }
+
+    public boolean filterDone(@NonNull Context context) {
+        if (categoriesDisplayed.size() == 1) {
+            Category category = categoriesDisplayed.get(0);
+            Example[] examples = category.getExamples();
+            if (examples.length == 1) {
+                Example example = examples[0];
+                onExampleSelected(context, example);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void filter(@NonNull Context context,
+                       @NonNull String query) {
+
+        String queryLC = query.trim()
+                .toLowerCase(Locale.ENGLISH);
+
+        List<Category> categories = new LinkedList<>();
+
+        if (queryLC.length() == 0) {
+            categories.addAll(categoriesOriginal);
+        } else {
+            for (Category category : categoriesOriginal) {
+                List<Example> filtered = new LinkedList<>();
+                Example[] examples = category.getExamples();
+
+                // If the category matches the query, add all examples then move to the next item
+                String categoryName = context.getString(category.getName())
+                        .toLowerCase(Locale.ENGLISH);
+
+                if (categoryName.contains(queryLC)) {
+                    categories.add(category);
+                    continue;
+                }
+
+                // Iterate the examples adding matches to the filtered list
+                for (Example example : examples) {
+                    String exampleName = context.getString(example.getName())
+                            .toLowerCase(Locale.ENGLISH);
+
+                    if (exampleName.contains(queryLC)) {
+                        filtered.add(example);
+                    }
+                }
+
+                // If the category has any matching examples, keep it and add the matches
+                final int size = filtered.size();
+                if (size > 0) {
+                    Example[] filteredExamples = filtered.toArray(new Example[size]);
+                    Category filteredCategory = new Category(category.getName(), filteredExamples);
+                    categories.add(filteredCategory);
+                }
+            }
+        }
+
+        setReferencesFromList(categories);
+    }
+
     public void setCategories(@NonNull List<Category> categories) {
-        final List<NamedIndexReference<INamed>> indexReferences = new LinkedList<>();
+        this.categoriesOriginal = categories;
+        setReferencesFromList(categories);
+    }
+
+    void setReferencesFromList(List<Category> categories) {
+        final List<NamedIndexReference<INamed>> indexReferences;
+        indexReferences = new LinkedList<>();
         for (Category category : categories) {
             indexReferences.add(new CategoryReference(category));
             Example[] examples = category.getExamples();
@@ -69,26 +152,20 @@ public final class CategoryAdapter extends ReferencedAdapter<INamed, NamedRefere
             }
         }
 
+        categoriesDisplayed = categories;
         setReferences(indexReferences);
     }
 
-    @Override
-    public void onReferenceClicked(View view, IndexReference reference) {
-        Class type = reference.getType();
-        if (Example.class.equals(type)) {
-            Example example = (Example) reference.get();
-            Class aClass = example.getType();
-            Context context = view.getContext();
+    void onExampleSelected(@NonNull Context context,
+                           @NonNull Example example) {
 
-            if (Activity.class.isAssignableFrom(aClass)) {
-                context.startActivity(new Intent(context, aClass));
-            } else {
-                Intent intent = new Intent(context, ExamplesActivity.class);
-                intent.putExtra(ExamplesActivity.EXTRA_EXAMPLE, example);
-                context.startActivity(intent);
-            }
+        Class aClass = example.getType();
+        if (Activity.class.isAssignableFrom(aClass)) {
+            context.startActivity(new Intent(context, aClass));
         } else {
-            throw new IllegalStateException("Unhandled reference for " + type);
+            Intent intent = new Intent(context, ExamplesActivity.class);
+            intent.putExtra(ExamplesActivity.EXTRA_EXAMPLE, example);
+            context.startActivity(intent);
         }
     }
 
