e1994154b5d42fd71a4d3867274b1e1c06067a20
Jared Woolston <jwoolston@tenkiv.com>
2016-03-23 19:38:41 -0700

    #1513 Adjusts dimensions to be 1 unit on each axis, simplifying scale calculations.
    Signed-off-by: Jared Woolston <jwoolston@tenkiv.com>

--- rajawali/src/main/java/org/rajawali3d/debug/CoordinateTrident.java
+++ rajawali/src/main/java/org/rajawali3d/debug/CoordinateTrident.java
@@ -23,44 +23,44 @@ public class CoordinateTrident extends Object3D {
         addChild(cube);
 
         // Axis Arms
-        final Cylinder xAxis = new Cylinder(1f, 0.05f, 1, 6);
+        final Cylinder xAxis = new Cylinder(0.75f, 0.05f, 1, 6);
         xAxis.setMaterial(material);
         xAxis.setColor(0xffff0000); // Red
         xAxis.rotate(Vector3.Axis.Y, 90.0);
-        xAxis.setPosition(0.5, 0, 0);
+        xAxis.setPosition(0.375, 0, 0);
         addChild(xAxis);
 
         final NPrism xTip = new NPrism(8, 0, 0.1f, 0.25f);
         xTip.setMaterial(material);
         xTip.setColor(0xffff0000); // Red
         xTip.rotate(Vector3.Axis.Z, 90.0);
-        xTip.setPosition(1.125f, 0, 0);
+        xTip.setPosition(0.875f, 0, 0);
         addChild(xTip);
 
-        final Cylinder yAxis = new Cylinder(1f, 0.05f, 1, 6);
+        final Cylinder yAxis = new Cylinder(0.75f, 0.05f, 1, 6);
         yAxis.setMaterial(material);
         yAxis.setColor(0xff00ff00); // Green
         yAxis.rotate(Vector3.Axis.X, 90.0);
-        yAxis.setPosition(0, 0.5, 0);
+        yAxis.setPosition(0, 0.375, 0);
         addChild(yAxis);
 
         final NPrism yTip = new NPrism(8, 0, 0.1f, 0.25f);
         yTip.setMaterial(material);
         yTip.setColor(0xff00ff00); // Green
-        yTip.setPosition(0, 1.125f, 0);
+        yTip.setPosition(0, 0.875f, 0);
         addChild(yTip);
 
-        final Cylinder zAxis = new Cylinder(1f, 0.05f, 1, 6);
+        final Cylinder zAxis = new Cylinder(0.75f, 0.05f, 1, 6);
         zAxis.setMaterial(material);
         zAxis.setColor(0xff0000ff); // Blue
-        zAxis.setPosition(0, 0, 0.5);
+        zAxis.setPosition(0, 0, 0.375);
         addChild(zAxis);
 
         final NPrism zTip = new NPrism(8, 0, 0.1f, 0.25f);
         zTip.setMaterial(material);
         zTip.setColor(0xff0000ff); // Blue
         zTip.rotate(Vector3.Axis.X, -90.0);
-        zTip.setPosition(0, 0, 1.125f);
+        zTip.setPosition(0, 0, 0.875f);
         addChild(zTip);
     }
 }
