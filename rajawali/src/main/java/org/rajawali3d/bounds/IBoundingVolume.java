b08b992320aa09851416934e2ce70341d65c02cd
Jared Woolston <Jared.Woolston@gmail.com>
2017-06-28 19:03:41 -0700

    Updates BoundingBox to provided a centroid location.
    Signed-off-by: Jared Woolston <Jared.Woolston@gmail.com>

--- rajawali/src/main/java/org/rajawali3d/bounds/IBoundingVolume.java
+++ rajawali/src/main/java/org/rajawali3d/bounds/IBoundingVolume.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2013 Dennis Ippel
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -16,17 +16,19 @@ import org.rajawali3d.Object3D;
 import org.rajawali3d.cameras.Camera;
 import org.rajawali3d.Geometry3D;
 import org.rajawali3d.math.Matrix4;
+import org.rajawali3d.math.vector.Vector3;
 
 public interface IBoundingVolume {
-	
+
 	public static final int DEFAULT_COLOR = 0xFFFFFF00;
-	
+
 	public void calculateBounds(Geometry3D geometry);
-	public void drawBoundingVolume(Camera camera, final Matrix4 vpMatrix, final Matrix4 projMatrix, 
+	public void drawBoundingVolume(Camera camera, final Matrix4 vpMatrix, final Matrix4 projMatrix,
 			final Matrix4 vMatrix, final Matrix4 mMatrix);
 	public void transform(Matrix4 matrix);
 	public boolean intersectsWith(IBoundingVolume boundingVolume);
-	
+	public Vector3 getPosition();
+
 	public Object3D getVisual();
 	public void setBoundingColor(int color);
 	public int getBoundingColor();
