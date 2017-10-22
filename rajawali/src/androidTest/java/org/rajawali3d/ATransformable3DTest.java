a575e46f65fc237e80ba0aca4cd11b0e47f79993
contriteobserver <contriteobserver@github.com>
2017-10-21 21:19:31 -0700

    added getScale/setScale and getLookAt/setLookAt tests

--- rajawali/src/androidTest/java/org/rajawali3d/ATransformable3DTest.java
+++ rajawali/src/androidTest/java/org/rajawali3d/ATransformable3DTest.java
@@ -5,6 +5,7 @@ import org.junit.*;
 
 import java.lang.Math;
 import org.rajawali3d.ATransformable3D;
+import org.rajawali3d.math.vector.Vector3;
 
 /**
  * @author Jared Woolston (jwoolston@keywcorp.com)
@@ -26,6 +27,24 @@ public class ATransformable3DTest {
     }
 
     @Test
+    public void testGetScale() throws Exception {
+        double expected = 1;
+        Vector3 scale = transformable.getScale();
+        assertEquals(expected, scale.x, 1e-10);
+        assertEquals(expected, scale.y, 1e-10);
+        assertEquals(expected, scale.z, 1e-10);
+    }
+
+    @Test
+    public void testGetLookAt() throws Exception {
+        double expected = 0;
+        Vector3 rotation = transformable.getLookAt();
+        assertEquals(expected, rotation.x, 1e-10);
+        assertEquals(expected, rotation.y, 1e-10);
+        assertEquals(expected, rotation.z, 1e-10);
+    }
+
+    @Test
     public void testGetRotX() throws Exception {
         double expected = 0;
         assertEquals(expected, transformable.getRotX(), 1e-10);
@@ -41,6 +60,30 @@ public class ATransformable3DTest {
     public void testGetRotZ() throws Exception {
         double expected = 0;
         assertEquals(expected, transformable.getRotZ(), 1e-10);
+    }
+
+    @Test
+    public void testSetScale() throws Exception {
+        double expectedX = 60;
+        double expectedY = 120;
+        double expectedZ = 180;
+        transformable.setScale(expectedX, expectedY, expectedZ);
+        Vector3 scale = transformable.getScale();
+        assertEquals(expectedX, scale.x, 1e-10);
+        assertEquals(expectedY, scale.y, 1e-10);
+        assertEquals(expectedZ, scale.z, 1e-10);
+    }
+
+    @Test
+    public void testSetLookAt() throws Exception {
+        double expectedX = 60;
+        double expectedY = 120;
+        double expectedZ = 180;
+        transformable.setLookAt(expectedX, expectedY, expectedZ);
+        Vector3 rotation = transformable.getLookAt();
+        assertEquals(expectedX, rotation.x, 1e-10);
+        assertEquals(expectedY, rotation.y, 1e-10);
+        assertEquals(expectedZ, rotation.z, 1e-10);
     }
 
     @Test
