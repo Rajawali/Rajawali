594f406d30997b11504b93fb80076ba5b8c5305b
Hannes Achleitner <hannes.software@gmx.at>
2020-11-08 08:18:46 +0100

    Rotate example

---
+++ examples/src/main/java/org/rajawali3d/examples/utils/DoubleUtils.kt
@@ -0,0 +1,10 @@
+package com.bmwgroup.apinext.facedetection.utils
+
+import kotlin.math.round
+
+fun Double.round(decimals: Int): Double {
+    var multiplier = 1.0
+    repeat(decimals) { multiplier *= 10 }
+    return round(this * multiplier) / multiplier
+}
+
