48109cf5a0baaee07eecc9f9ed7c85cf79cbb18b
contriteobserver <contriteobserver@gmail.com>
2022-01-07 16:20:27 -0800

    Added a checkbox to control the directional light animation.
    partially addresses #2328

--- examples/src/main/java/org/rajawali3d/examples/examples/ui/UIElementsFragment.java
+++ examples/src/main/java/org/rajawali3d/examples/examples/ui/UIElementsFragment.java
@@ -1,18 +1,22 @@
 package org.rajawali3d.examples.examples.ui;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import android.view.Gravity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.CheckBox;
 
 import org.rajawali3d.Object3D;
+import org.rajawali3d.animation.Animation;
+import org.rajawali3d.animation.Animation3D;
+import org.rajawali3d.animation.EllipticalOrbitAnimation3D;
 import org.rajawali3d.examples.R;
 import org.rajawali3d.examples.examples.AExampleFragment;
 import org.rajawali3d.lights.DirectionalLight;
@@ -20,30 +24,28 @@ import org.rajawali3d.loader.LoaderAWD;
 import org.rajawali3d.materials.Material;
 import org.rajawali3d.materials.methods.DiffuseMethod;
 import org.rajawali3d.materials.methods.SpecularMethod;
+import org.rajawali3d.math.vector.Vector3;
 
 public class UIElementsFragment extends AExampleFragment {
+    CheckBox selector;
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mRenderer instanceof UIElementsRenderer) {
+                UIElementsRenderer renderer = (UIElementsRenderer) mRenderer;
+                renderer.animate(selector.isChecked());
+            }
+        }
+    };
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        LinearLayout ll = new LinearLayout(getActivity());
-        ll.setOrientation(LinearLayout.VERTICAL);
-        ll.setGravity(Gravity.CENTER);
-
-        TextView label = new TextView(getActivity());
-        label.setText(R.string.ui_elements_fragment_text_view_halo_dunia);
-        label.setTextSize(20);
-        label.setGravity(Gravity.CENTER);
-        label.setHeight(100);
-        ll.addView(label);
-
-        ImageView image = new ImageView(getActivity());
-        image.setImageResource(R.drawable.rajawali_outline);
-        ll.addView(image);
-
-        mLayout.addView(ll);
-
+        ConstraintLayout overlay = (ConstraintLayout) inflater.inflate(R.layout.ui_overlay, container, false);
+        mLayout.addView(overlay);
+        selector = mLayout.findViewById(R.id.ui_selector);
+        selector.setOnClickListener(listener);
         return mLayout;
     }
 
@@ -52,9 +54,18 @@ public class UIElementsFragment extends AExampleFragment {
         return new UIElementsRenderer(getActivity(), this);
     }
 
-    private final class UIElementsRenderer extends AExampleRenderer {
-        private DirectionalLight mLight;
+    private static final class UIElementsRenderer extends AExampleRenderer {
+        private Animation3D anim;
         private Object3D mMonkey;
+
+        void animate(boolean play) {
+            if(anim == null) return;
+            if(play) {
+                anim.play();
+            } else {
+                anim.pause();
+            }
+        }
 
         UIElementsRenderer(Context context, @Nullable AExampleFragment fragment) {
             super(context, fragment);
@@ -62,9 +73,7 @@ public class UIElementsFragment extends AExampleFragment {
 
         @Override
         protected void initScene() {
-            mLight = new DirectionalLight(0, 0, -1);
-            mLight.setPower(.8f);
-
+            DirectionalLight mLight = new DirectionalLight(1, 0, 0);
             getCurrentScene().addLight(mLight);
             getCurrentCamera().setPosition(0, 0, 8);
 
@@ -79,12 +88,22 @@ public class UIElementsFragment extends AExampleFragment {
                 e.printStackTrace();
             }
 
+            anim = new EllipticalOrbitAnimation3D(
+                    new Vector3(0, 0f, 0), new Vector3(-1, 0, 0), 0, 180);
+            anim.setRepeatMode(Animation.RepeatMode.REVERSE_INFINITE);
+            anim.setInterpolator(new AccelerateDecelerateInterpolator());
+            anim.setDurationDelta(6);
+            anim.setTransformable3D(mLight);
+            getCurrentScene().registerAnimation(anim);
+            mLight.enableLookAt(); // allows animation to update orientation
+            anim.play();
+
             Material material = new Material();
             material.enableLighting(true);
             material.setDiffuseMethod(new DiffuseMethod.Lambert());
             material.setSpecularMethod(new SpecularMethod.Phong());
             mMonkey.setMaterial(material);
-            mMonkey.setColor(0xff99C224);
+            mMonkey.setColor(Color.YELLOW & Color.GRAY);
         }
     }
 }
