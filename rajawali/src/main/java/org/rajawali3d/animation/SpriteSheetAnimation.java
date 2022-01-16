753e38f4bf50b2b05ee795488336e0c53da61ff1
contriteobserver <contriteobserver@gmail.com>
2022-01-16 11:09:40 -0800

    implements `SpriteSheetAnimation` a texture based animation. Also provides greater access to `ATexture` transforms
    addresses issue #2416

---
+++ rajawali/src/main/java/org/rajawali3d/animation/SpriteSheetAnimation.java
@@ -0,0 +1,65 @@
+package org.rajawali3d.animation;
+
+import static org.rajawali3d.math.MathUtil.clamp;
+
+import android.renderscript.Matrix3f;
+
+import androidx.annotation.NonNull;
+
+import org.rajawali3d.animation.Animation;
+import org.rajawali3d.materials.textures.ATexture;
+
+public class SpriteSheetAnimation extends Animation {
+    private final ATexture mTexture;
+    private Matrix3f mTransform = new Matrix3f();
+    private int mCols = 1;
+    private int mRows = 1;
+    private int mStart = 0;
+    private int mEnd = 1;
+    private boolean mForward = true;
+
+    /* assumes sprite sheet is a grid of frames,
+       sequenced left to right, and top to bottom
+     */
+    public SpriteSheetAnimation(@NonNull ATexture texture, int numCols, int numRows) {
+        super();
+        texture.enableTransforms(true);
+        mTexture = texture;
+        if(numCols > 0) mCols = numCols;
+        if(numRows > 0) mRows = numRows;
+        mEnd = mCols * mRows -1;
+    }
+
+    /* selects range of frames to animate,
+       if startFrame is greater than endFrame,
+       sprite is animated in reverse
+     */
+    public void selectRange(int startFrame, int endFrame) {
+        mStart = clamp(startFrame, 0, mCols*mRows-1);
+        mEnd = clamp(endFrame, 0, mCols*mRows-1);
+        mForward = mEnd > mStart;
+    }
+
+    @Override
+    protected void applyTransformation() {
+        mTransform.loadIdentity();
+        mTransform.translate(selectCol()/(float)mCols,selectRow()/(float)mRows);
+        mTransform.scale(1/(float)mCols,1/(float)mRows);
+        mTexture.applyTransform(mTransform);
+    }
+
+    private long selectCol() {
+        double t = mForward ?
+                Math.abs(mStart + mInterpolatedTime * mEnd) :
+                Math.abs(mStart - mInterpolatedTime * (mStart-mEnd));
+        return Math.round(t) % mCols;
+    }
+
+    private long selectRow() {
+        double t = mForward ?
+                Math.abs(mStart + mInterpolatedTime * mEnd) :
+                Math.abs(mStart - mInterpolatedTime * (mStart-mEnd));
+        return Math.round(t) / mCols;
+    }
+}
+
