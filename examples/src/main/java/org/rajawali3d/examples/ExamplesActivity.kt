36d8fa6f1d9e64a3521fb64f213b6ac7990f6130
Hannes Achleitner <hannes.software@gmx.at>
2022-12-20 17:25:58 +0100

    Back button

--- examples/src/main/java/org/rajawali3d/examples/ExamplesActivity.kt
+++ examples/src/main/java/org/rajawali3d/examples/ExamplesActivity.kt
@@ -1,6 +1,8 @@
 package org.rajawali3d.examples
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.rajawali3d.examples.data.Example
@@ -10,9 +12,24 @@ class ExamplesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_examples)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setHomeButtonEnabled(true)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
         if (savedInstanceState == null) {
             val extras = intent.extras
             extras?.let { processExtras(it) }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
