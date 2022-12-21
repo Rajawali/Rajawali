63153891918ee2f698f6a934a2e7f58ffbdae49d
Hannes Achleitner <hannes.software@gmx.at>
2022-12-21 11:52:19 +0100

    Remove sample button
    It prevents from back press in Espresso tests

--- examples/src/main/java/org/rajawali3d/examples/examples/AExampleFragment.java
+++ examples/src/main/java/org/rajawali3d/examples/examples/AExampleFragment.java
@@ -1,9 +1,6 @@
 package org.rajawali3d.examples.examples;
 
-import android.content.ActivityNotFoundException;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.CallSuper;
 import androidx.annotation.LayoutRes;
@@ -14,13 +11,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 
 import org.rajawali3d.examples.R;
-import org.rajawali3d.examples.views.GitHubLogoView;
 import org.rajawali3d.renderer.ISurfaceRenderer;
 import org.rajawali3d.renderer.Renderer;
 import org.rajawali3d.view.IDisplay;
@@ -29,29 +24,12 @@ import org.rajawali3d.view.ISurface;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-public abstract class AExampleFragment extends Fragment implements IDisplay, OnClickListener {
-
-    public static final String BUNDLE_EXAMPLE_URL = "BUNDLE_EXAMPLE_URL";
+public abstract class AExampleFragment extends Fragment implements IDisplay {
 
     protected ProgressBar mProgressBarLoader;
-    protected GitHubLogoView mImageViewExampleLink;
-    private String mExampleUrl;
     protected FrameLayout mLayout;
     protected ISurface mRenderSurface;
     protected ISurfaceRenderer mRenderer;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        final Bundle bundle = getArguments();
-        if (bundle == null || !bundle.containsKey(BUNDLE_EXAMPLE_URL)) {
-            throw new IllegalArgumentException(getClass().getSimpleName()
-                    + " requires " + BUNDLE_EXAMPLE_URL + " argument at runtime!");
-        }
-
-        mExampleUrl = bundle.getString(BUNDLE_EXAMPLE_URL);
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -69,10 +47,6 @@ public abstract class AExampleFragment extends Fragment implements IDisplay, OnC
         mProgressBarLoader = mLayout.findViewById(R.id.progress_bar_loader);
         mProgressBarLoader.setVisibility(View.GONE);
 
-        // Set the example link
-        mImageViewExampleLink = mLayout.findViewById(R.id.image_view_example_link);
-        mImageViewExampleLink.setOnClickListener(this);
-
         // Create the renderer
         mRenderer = createRenderer();
         onBeforeApplyRenderer();
@@ -86,20 +60,6 @@ public abstract class AExampleFragment extends Fragment implements IDisplay, OnC
     @CallSuper
     protected void applyRenderer() {
         mRenderSurface.setSurfaceRenderer(mRenderer);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.image_view_example_link) {
-            if (mImageViewExampleLink == null)
-                throw new IllegalStateException("Example link is null!");
-
-            try {
-                final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mExampleUrl));
-                startActivity(intent);
-            } catch (ActivityNotFoundException ignored) {
-            }
-        }
     }
 
     @Override
