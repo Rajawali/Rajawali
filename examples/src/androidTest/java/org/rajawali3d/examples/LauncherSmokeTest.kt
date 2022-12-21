9b1ae089446e5a7db3971ebcf3a4be496d681c40
Hannes Achleitner <hannes.software@gmx.at>
2022-12-21 08:10:15 +0100

    Smoketest any Fragment

--- examples/src/androidTest/java/org/rajawali3d/examples/LauncherSmokeTest.kt
+++ examples/src/androidTest/java/org/rajawali3d/examples/LauncherSmokeTest.kt
@@ -1,21 +1,28 @@
 package org.rajawali3d.examples
 
 import android.Manifest
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.takeScreenshot
 import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
 import org.junit.runner.RunWith
+import org.rajawali3d.examples.data.DataSet
+import org.rajawali3d.examples.tools.RecyclerViewMatcher
 
 @RunWith(AndroidJUnit4::class)
 class LauncherSmokeTest {
@@ -25,6 +32,12 @@ class LauncherSmokeTest {
 
     @get:Rule
     var nameRule = TestName()
+
+    @Before
+    fun setUp() = Intents.init()
+
+    @After
+    fun cleanUp() = Intents.release()
 
     @get:Rule
     val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
@@ -40,8 +53,29 @@ class LauncherSmokeTest {
             .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-A")
 
         onView(withId(R.id.recycler)).check(matches(isDisplayed()))
-        onView(ViewMatchers.isRoot())
+        onView(isRoot())
             .captureToBitmap()
             .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-R")
+
+        var overallIndex = 0
+        DataSet.instance?.categories?.forEach {
+            it.examples.forEach { _ ->
+                overallIndex++
+                println("Click on $overallIndex")
+                clickOnExample(overallIndex)
+            }
+            overallIndex++
+        }
+    }
+
+    private fun clickOnExample(itemIndex: Int) {
+        onView(withId(R.id.recycler)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(itemIndex))
+
+        onView(RecyclerViewMatcher(R.id.recycler).atPositionOnView(itemIndex, R.id.textItem))
+            .check(matches(isDisplayed()))
+            .perform(click())
+        //  Intents.intended(hasComponent(ExamplesActivity::class.java.name))
+
+        Espresso.pressBack()
     }
 }
---
+++ examples/src/androidTest/java/org/rajawali3d/examples/tools/RecyclerViewMatcher.kt
@@ -0,0 +1,52 @@
+package org.rajawali3d.examples.tools
+
+import android.content.res.Resources
+import android.content.res.Resources.NotFoundException
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+class RecyclerViewMatcher(private val recyclerViewId: Int) {
+
+    fun atPosition(position: Int): Matcher<View> {
+        return atPositionOnView(position, -1)
+    }
+
+    fun atPositionOnView(position: Int, targetViewId: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            var resources: Resources? = null
+            var childView: View? = null
+            override fun describeTo(description: Description) {
+                var idDescription = recyclerViewId.toString()
+                if (resources != null) {
+                    idDescription = try {
+                        resources!!.getResourceName(recyclerViewId)
+                    } catch (var4: NotFoundException) {
+                        String.format("%s (resource name not found)", recyclerViewId)
+                    }
+                }
+                description.appendText("with id: $idDescription")
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                resources = view.resources
+                if (childView == null) {
+                    val recyclerView = view.rootView.findViewById<RecyclerView>(recyclerViewId)
+                    childView = if (recyclerView != null && recyclerView.id == recyclerViewId) {
+                        recyclerView.findViewHolderForAdapterPosition(position)!!.itemView
+                    } else {
+                        return false
+                    }
+                }
+                return if (targetViewId == -1) {
+                    view === childView
+                } else {
+                    val targetView = childView!!.findViewById<View>(targetViewId)
+                    view === targetView
+                }
+            }
+        }
+    }
+}
