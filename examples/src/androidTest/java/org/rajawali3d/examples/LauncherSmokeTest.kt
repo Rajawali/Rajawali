844fd70df2aac9e65931ad5b1c48913acd8bae0a
Hannes Achleitner <hannes.software@gmx.at>
2022-12-29 17:49:05 +0100

    Fix smoketest

--- examples/src/androidTest/java/org/rajawali3d/examples/LauncherSmokeTest.kt
+++ examples/src/androidTest/java/org/rajawali3d/examples/LauncherSmokeTest.kt
@@ -1,5 +1,7 @@
 package org.rajawali3d.examples
 
+import android.Manifest
+import androidx.test.core.app.takeScreenshot
 import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -9,6 +11,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
@@ -23,11 +26,22 @@ class LauncherSmokeTest {
     @get:Rule
     var nameRule = TestName()
 
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
     @Test
     fun smokeTestSimplyStart() {
+        Thread.sleep(300)
+        takeScreenshot()
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-A")
+
         onView(withId(R.id.recycler)).check(matches(isDisplayed()))
         onView(ViewMatchers.isRoot())
             .captureToBitmap()
-            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}")
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-R")
     }
 }
